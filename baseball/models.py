976fd5890339543e96248237ebfd414e57d7f802
terryseyler <tlseyler@gmail.com>
2023-02-26 21:44:04 -0500

    created model

--- baseball/models.py
+++ baseball/models.py
@@ -7,9 +7,11 @@ class Stadiums(models.Model):
     timezone = models.IntegerField(default=0)
     latitude = models.FloatField(default=0)
     longitude = models.FloatField(default=0)
-    city_id = models.IntegerField(primary_key = True)
+    city_id = models.IntegerField(default = 0)
     stadium_name =  models.CharField(max_length=200,primary_key=True)
     home_team = models.CharField(max_length=200)
+    def __str__(self):
+        return self.stadium_name
 
 class Forecast(models.Model):
     city_id = models.CharField(max_length=200)
@@ -20,15 +22,15 @@ class Forecast(models.Model):
     feels_like = models.FloatField(default = 0)
     temp_min = models.FloatField(default=0)
     temp_max = models.FloatField(default=0)
-    pressure = Imodels.ntegerField(default=0)
+    pressure = models.IntegerField(default=0)
     sea_level = models.IntegerField(default=0)
     grnd_level = models.IntegerField(default=0)
     humidity = models.IntegerField(default=0)
     temp_kf = models.FloatField(default=0)
 
     weather_id = models.IntegerField(default=0)
-    weather_main = models.CharField(max_lenth = 200)
-    weather_description = models.CharField(max_lenth = 200)
+    weather_main = models.CharField(max_length = 200)
+    weather_description = models.CharField(max_length = 200)
     clouds_all = models.IntegerField(default=0)
 
     wind_speed = models.FloatField(default=0)
@@ -37,7 +39,7 @@ class Forecast(models.Model):
 
     visibility = models.FloatField(default=0)
 
-    df_txt = models.CharField(max_lenth = 200)
+    df_txt = models.CharField(max_length = 200)
 
 class GameSchedule(models.Model):
     game_id = models.CharField(max_length=200,primary_key=True)
@@ -46,10 +48,10 @@ class GameSchedule(models.Model):
     home_team = models.CharField(max_length=200)
     away_team = models.CharField(max_length=200)
     start_date = models.CharField(max_length=200)
-    stadium_name=models.ForeignKey(Stadiums,on_delete=CASCADE)
+    stadium_name=models.ForeignKey(Stadiums,on_delete=models.CASCADE)
 
 class Odds(models.Model):
-    game_id = model.ForeignKey(GameSchedule,on_delete=CASCADE)
+    game_id = models.ForeignKey(GameSchedule,on_delete=models.CASCADE)
     bookmaker = models.CharField(max_length=200)
     bookmaker_title = models.CharField(max_length=200)
     last_update = models.CharField(max_length=200)
