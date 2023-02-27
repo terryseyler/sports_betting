44224985c4cfa7d8a16fac4bbea6f11c42aa4d4c
terryseyler <tlseyler@gmail.com>
2023-02-27 15:47:13 -0500

    adjusted admin file

--- baseball/admin.py
+++ baseball/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from baseball.models import Stadiums,Forecast,GameSchedule,Odds
 # Register your models here.
+admin.site.register(Stadiums)
+admin.site.register(Forecast)
+admin.site.register(GameSchedule)
+admin.site.register(Odds)
